d44e4fd99941e73dd6fa2c550865210588a61529
b1tt0 <thssthfx@gmail.com>
2021-03-27 22:26:02 +0300

    lotta changes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
-import express from "express";
-import path from "path";
-import config from "./config.json";
+// imports
+import http        from "http";
+import express     from "express";
+import compression from "compression";
+import path        from "path";
+import config      from "./config.json";
 
-const app: express.Application = express();
+const app   : express.Application = express();
+// for fancy http server features
+const server: http.Server         = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, "views")));
+app.use(compression()); // use gzip
+
+// use all files from the views folder
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.render("index");
+// http://localhost:port/ route
+app.get("/", (r: express.Request, s: express.Response) => {
+  s.render("index");
 });
 
+// start up the website
 app.listen(config.website.port, () => {
   console.log(`website started @${config.website.port}`);
 });
