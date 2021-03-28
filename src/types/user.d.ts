692d3e6dc768dd711cd9dff1d396bc63463c373f
b1tt0 <thssthfx@gmail.com>
2021-03-28 15:43:24 +0300

    fixed for sure

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,8 +1,7 @@
-import Server from "server";
 // user account type
 
 export type User = {
   name   : string;          // user name
   id     : number;          // user id
-  servers: Server[] | null; // servers that the user owns
+  servers: import("./server").Server[] | null; // servers that the user owns
 }
