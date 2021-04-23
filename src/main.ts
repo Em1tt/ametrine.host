32e1c0c2760a7d8ef50240a4a4dfdf039e97f7ad
b1tt0 <thssthfx@gmail.com>
2021-04-23 20:29:18 +0300

    types and started preloader

--- src/main.ts
+++ src/main.ts
@@ -12,25 +12,12 @@ import { util } from "./util";
 // variables
 let modules: Module[];
 
-// preload all modules
+// preload all modules & start them
 
 
 process.on("SIGINT", () => {
-  // stop http server
-  server.close(() => {
-    util.expressLog(`http server stopped`)
-  });
-
-  // have to do this because nodejs hangs otherwise (could have done it with if (...) return)
-  if (!process.argv.includes("--no-redis")) {
-    // disconnect redis client
-    db.client.quit();
-    util.redisLog(`client disconnected`);
-    // stop redis server
-    db.server.close((e) => {
-      util.redisLog(`server closed`)
-    });
-  }
+  // stop every module
+  for (let m of modules) { m.stop() }
 
   // close nodejs
   process.exit(0);
--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -1,2 +1,7 @@
 // module type for safety
 
+export type Module = {
+  name : string;
+  start: (util: JSON) => void;
+  stop : () => void;
+}
