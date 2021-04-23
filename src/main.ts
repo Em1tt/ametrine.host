d893a697534d2b61e3b5b16a705a42b565ff76f6
b1tt0 <thssthfx@gmail.com>
2021-04-23 20:36:32 +0300

    finish preloader

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,35 @@
 // NOTE: to run with args, use `npm run start -- --arg`
 // imports
 import "dotenv/config";
-import fs from "fs";
-import Module from "./types/module";
+import path          from "path";
+import fs            from "fs";
+import child_process from "child_process";
+import Module        from "./types/module";
 
 // files
 import config   from "./config.json";
 import { util } from "./util";
 
 // variables
+const MODULE_PATH: string = path.join(__dirname, "modules");
 let modules: Module[];
 
-// preload all modules & start them
+// preload all modules
+fs.readdir(MODULE_PATH, (e, f) => {
+  if (e) throw new Error(e);
+  f.forEach((file) => modules.push(import(file)));
+});
 
+// start all modules in separate processes
+modules.forEach((m) => {
+  child_process.spawn(process.argv[0], [m], {
+    detached: true // make it independent
+  });
+});
 
 process.on("SIGINT", () => {
   // stop every module
-  for (let m of modules) { m.stop() }
+  modules.forEach((m) => m.stop());
 
   // close nodejs
   process.exit(0);
