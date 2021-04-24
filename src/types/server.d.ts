af221c9c9dc72c10d101cd937ee8233df51c94f3
b1tt0 <thssthfx@gmail.com>
2021-04-24 11:25:27 +0300

    ready to merge with main!

---
+++ src/types/server.d.ts
@@ -0,0 +1,8 @@
+// user server type
+export type Server = {
+  name : string;
+  id   : number;
+  owner: number; // server owner id
+  price: number;
+  ip   : string;
+}
---
+++ src/types/user.d.ts
@@ -0,0 +1,7 @@
+// user account type
+import Server from "./server";
+export type User = {
+  name    : string;
+  id      : number;
+  servers?: Array<Server>;
+}
