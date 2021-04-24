eb264d21def3ec18c658d269a4cb253a674fff6c
b1tt0 <thssthfx@gmail.com>
2021-04-24 11:17:34 +0300

    custom discord typings (em1t is lazy)

--- src/modules/discord.ts
+++ src/modules/discord.ts
@@ -1,7 +1,8 @@
 // discord bot!!
 // i will let @Em1t do all the fancy ts stuff here
-import "dotenv/config"
+import "dotenv/config";
 import Discord  from "discord.js";
+import Command  from "types/command.d.ts";
 import { util } from "../util";
 
 const bot = new Discord.Client();
