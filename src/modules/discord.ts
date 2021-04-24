d5ce7717003ba6d35851704b6b027bd61350e458
b1tt0 <thssthfx@gmail.com>
2021-04-24 11:42:18 +0300

    a little trolling

--- src/modules/discord.ts
+++ src/modules/discord.ts
@@ -2,7 +2,7 @@
 // i will let @Em1t do all the fancy ts stuff here
 import "dotenv/config";
 import Discord  from "discord.js";
-import Command  from "./types/command";
+import Command  from "../types/command";
 import { util } from "../util";
 
 const bot = new Discord.Client();
