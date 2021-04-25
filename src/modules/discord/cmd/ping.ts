c4c2a9a97b41c2b72a31baee1db70f7cd8ee4417
b1tt0 <thssthfx@gmail.com>
2021-04-25 19:03:54 +0300

    resolve 3 warnings

--- src/modules/discord/cmd/ping.ts
+++ src/modules/discord/cmd/ping.ts
@@ -1,10 +1,13 @@
 // ping command
 // should just write "pong"
 import { Command } from "../../../types/discord/command";
+import { Client, Message } from "discord.js";
 export const prop = {
   name : "ping",
   desc : "Check if the bot is alive.",
   usage: "ping",
 
-  run: (bot, msg, args) => msg.reply("Pong!")
+  run: (bot: Client, 
+        msg: Message, 
+        args: Array<string>) => msg.reply("Pong!")
 }
