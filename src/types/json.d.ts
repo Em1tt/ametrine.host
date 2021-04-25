e4ea252949c698d17851bdb29b294baa4f79a719
b1tt0 <thssthfx@gmail.com>
2021-04-25 10:52:29 +0300

    crippling depression

--- src/types/json.d.ts
+++ src/types/json.d.ts
@@ -1,12 +1,16 @@
 // for importing .json files
+/* this was the cooler version that didn't work
 type Value = string       |
              number       |
              boolean      |
              null         |
              Array<Value> |
              { [Key: string]: Value };
+*/
 
 declare module "*.json" {
-  const          value: Value;
+  // please don't use any
+  // always try to do stuff the cool ts way
+  const          value: /* Value */ any;
   export default value;
 }
