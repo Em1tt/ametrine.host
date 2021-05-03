d77029f67341217cda1a0e43c3a580641394d7b0
b1tt0 <thssthfx@gmail.com>
2021-05-03 17:41:04 +0300

    ...

--- src/modules/discord/cmd/bash.ts
+++ src/modules/discord/cmd/bash.ts
@@ -3,7 +3,7 @@ import { Client, Message } from "discord.js";
 import child_process       from "child_process";
 import config from "../../../config.json";
 
-let error: boolean = false;
+let error = false;
 
 export const prop = {
   name : "bash",
@@ -11,27 +11,27 @@ export const prop = {
   usage: "bash [command] [...args]",
 
   run: async (bot: Client, msg: Message, args: Array<string>): Promise<void> => { 
-  	if (!config.discord.dev.includes(msg.author.id)) return;
+  if (!config.discord.dev.includes(msg.author.id)) return;
 
 	const cmd : string = args[0];
 	args.shift();
-	const sh  : string = "```\n# " + cmd + " " + args.join(" ");
-	let   data: string = "";
+	const sh           = "```\n# " + cmd + " " + args.join(" ");
+  let   data         = "";
 
 	const m       : Message = await msg.channel.send(sh);
 	const exec    : any     = child_process.spawn(cmd, args);
 	const interval: any     = setInterval(m.edit, 1000,
-	  								  sh + "\n" + data + "```"
-	  								 );
+                                        sh + "\n" + data + "```"
+                                       );
 
 	exec.stdout.on("data", (d) => data += d);
 	exec.on("error", (e) => error = true);
 	exec.on("close", async (c) => {
-	  clearInterval(interval);
-	  if (error) {
-	    await m.edit("Err");
-	    return error = false;
-	  } else await m.edit(sh + "\n" + data + "\n#```");
-	});
+    clearInterval(interval);
+    if (error) {
+      await m.edit("Err");
+      return error = false;
+    } else await m.edit(sh + "\n" + data + "\n#```");
+  });
   }
 }
