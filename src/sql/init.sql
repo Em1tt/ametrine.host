21287075aa595a0f7f77851ef3d489d15859ec8b
b1tt0 <thssthfx@gmail.com>
2021-05-11 21:11:29 +0300

    invoices

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -1,12 +1,10 @@
 -- initialize all tables
--- users
 CREATE TABLE IF NOT EXISTS users (
   id         INTEGER   PRIMARY KEY,
   registered TIMESTAMP NOT NULL,
   name       TEXT      NOT NULL
 );
 
--- servers
 CREATE TABLE IF NOT EXISTS servers (
   id       INTEGER   PRIMARY KEY,
   opened   TIMESTAMP NOT NULL,
@@ -14,10 +12,17 @@ CREATE TABLE IF NOT EXISTS servers (
   location TEXT      NOT NULL DEFAULT "na" -- eu, us...
 );
 
--- receipts
 CREATE TABLE IF NOT EXISTS receipts (
   id       INTEGER   PRIMARY KEY,
   opened   TIMESTAMP NOT NULL,
+  price    FLOAT     NOT NULL DEFAULT 0.00,
+  currency TEXT      NOT NULL DEFAULT '€'
+);
+
+CREATE TABLE IF NOT EXISTS invoices (
+  id       INTEGER   PRIMARY KEY,
+  opened   TIMESTAMP NOT NULL,
+  due      TIMESTAMP NOT NULL,
   price    FLOAT     NOT NULL DEFAULT 0.00,
   currency TEXT      NOT NULL DEFAULT '€'
 );
