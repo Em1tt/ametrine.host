225067cf70ce0c738c66e395b508035c98850da3
b1tt0 <thssthfx@gmail.com>
2021-05-11 09:35:02 +0300

    fuck you

--- src/modules/website.ts
+++ src/modules/website.ts
@@ -1,13 +1,13 @@
 // imports
-import { Express as e } from "express";
-import compression      from "compression";
-import morgan           from "morgan";
-import minify           from "express-minify";
-import path             from "path";
-import fs 		          from "fs";
-import config           from "../config.json";
-import { util }         from "../util";
-const app : e.Application = e();
+import epress      from "express";
+import compression from "compression";
+import morgan      from "morgan";
+import minify      from "express-minify";
+import path        from "path";
+import fs 		     from "fs";
+import config      from "../config.json";
+import { util }    from "../util";
+const app : express.Application = express();
 const html: string = path.join(__dirname, "views", "html");
 
 app.use(morgan("[express]\t:method :url :status :res[content-length] - :response-time ms"));
@@ -17,21 +17,21 @@ app.use(compression());
 // minify static files
 app.use(minify());
 // serve static files
-app.use(e.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "views")));
 
-app.get("/", (r: e.Request, s: e.Response) => {
+app.get("/", (r: express.Request, s: express.Response) => {
   s.sendFile(`${html}/index.html`);
 });
 
 /* amethyst.host/api/bill
    amethyst.host/api/auth
    and so on..            */
-app.get("/api/:method", (r: e.Request, s: e.Response) => {
+app.get("/api/:method", (r: express.Request, s: express.Response) => {
   s.send(r.params.name);
 });
 
 // "smart" router
-app.get("/:name", (r: e.Request, s: e.Response) => {
+app.get("/:name", (r: express.Request, s: express.Response) => {
   const file = `${html}/${r.params.name}.html`;
 
   if (!fs.existsSync(file)) 
