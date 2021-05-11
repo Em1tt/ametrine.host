00c243185b75de01fecfbac0717ce2f6011b4620
b1tt0 <thssthfx@gmail.com>
2021-05-11 21:08:54 +0300

    receipts

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -1,15 +1,23 @@
 -- initialize all tables
 -- users
 CREATE TABLE IF NOT EXISTS users (
-  id         INTEGER PRIMARY KEY,
-  registered TEXT    NOT NULL,
-  name       TEXT    NOT NULL
+  id         INTEGER   PRIMARY KEY,
+  registered TIMESTAMP NOT NULL,
+  name       TEXT      NOT NULL
 );
 
 -- servers
 CREATE TABLE IF NOT EXISTS servers (
-  id       INTEGER PRIMARY KEY,
-  opened   TEXT    NOT NULL,
-  name     TEXT    NOT NULL,
-  location TEXT    NOT NULL DEFAULT "na" -- eu, us...
+  id       INTEGER   PRIMARY KEY,
+  opened   TIMESTAMP NOT NULL,
+  name     TEXT      NOT NULL,
+  location TEXT      NOT NULL DEFAULT "na" -- eu, us...
+);
+
+-- receipts
+CREATE TABLE IF NOT EXISTS receipts (
+  id       INTEGER   PRIMARY KEY,
+  opened   TIMESTAMP NOT NULL,
+  price    FLOAT     NOT NULL DEFAULT 0.00,
+  currency TEXT      NOT NULL DEFAULT '€'
 );
--- src/sql/register.sql
+++ src/sql/register.sql
@@ -1,5 +1,5 @@
 -- register a new user
-INSERT INTO users (id, created, name) VALUES(
+INSERT INTO users (id, registered, name) VALUES(
   -- get the most recent entry, it will have the highest id
   (SELECT * FROM users ORDER BY column DESC LIMIT 1) + 1,
   -- timestamp
