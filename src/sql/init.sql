57c380a9213aacf33052b1d0d502cb477f84714a
Fire <17692105+FireMario211@users.noreply.github.com>
2021-05-12 17:54:20 +0000

    Fix indentation

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -1,6 +1,6 @@
 -- initialize all tables
 CREATE TABLE IF NOT EXISTS users (
-  user_id         INTEGER   NOT NULL PRIMARY KEY,
+  user_id    INTEGER   NOT NULL PRIMARY KEY,
   registered TIMESTAMP NOT NULL,
   name       TEXT      NOT NULL,
   password   TEXT      NOT NULL,
@@ -8,16 +8,16 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS servers (
-  server_id       INTEGER   NOT NULL PRIMARY KEY,
-  opened   TIMESTAMP NOT NULL,
-  name     TEXT      NOT NULL,
-  location TEXT      NOT NULL DEFAULT "na" -- eu, us...
+  server_id  INTEGER   NOT NULL PRIMARY KEY,
+  opened     TIMESTAMP NOT NULL,
+  name       TEXT      NOT NULL,
+  location   TEXT      NOT NULL DEFAULT "na" -- eu, us...
 );
 
 CREATE TABLE IF NOT EXISTS invoices (
-  invoice_id       INTEGER   NOT NULL PRIMARY KEY,
-  opened   TIMESTAMP NOT NULL,
-  due      TIMESTAMP NOT NULL,
-  price    REAL      NOT NULL DEFAULT 0.00,
-  currency TEXT      NOT NULL DEFAULT '€'
+  invoice_id  INTEGER   NOT NULL PRIMARY KEY,
+  opened      TIMESTAMP NOT NULL,
+  due         TIMESTAMP NOT NULL,
+  price       REAL      NOT NULL DEFAULT 0.00,
+  currency    TEXT      NOT NULL DEFAULT '€'
 );
