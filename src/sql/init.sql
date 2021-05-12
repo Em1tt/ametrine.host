1a85d4d67d30eac5a367987e04dfeaa1b6bdfb2a
Fire <17692105+FireMario211@users.noreply.github.com>
2021-05-12 17:29:40 +0000

    Fix potential high risk issue

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -1,23 +1,26 @@
 -- initialize all tables
 CREATE TABLE IF NOT EXISTS users (
-  id         INTEGER   PRIMARY KEY,
+  id         INTEGER   NOT NULL DEFAULT 0,
   registered TIMESTAMP NOT NULL,
   name       TEXT      NOT NULL,
   password   TEXT      NOT NULL,
   salt       TEXT      NOT NULL
+  PRIMARY KEY("id")
 );
 
 CREATE TABLE IF NOT EXISTS servers (
-  id       INTEGER   PRIMARY KEY,
+  id       INTEGER   NOT NULL DEFAULT 0,
   opened   TIMESTAMP NOT NULL,
   name     TEXT      NOT NULL,
   location TEXT      NOT NULL DEFAULT "na" -- eu, us...
+  PRIMARY KEY("id")
 );
 
 CREATE TABLE IF NOT EXISTS invoices (
-  id       INTEGER   PRIMARY KEY,
+  id       INTEGER   NOT NULL DEFAULT 0,
   opened   TIMESTAMP NOT NULL,
   due      TIMESTAMP NOT NULL,
   price    REAL      NOT NULL DEFAULT 0.00,
   currency TEXT      NOT NULL DEFAULT '€'
+  PRIMARY KEY("id")
 );
