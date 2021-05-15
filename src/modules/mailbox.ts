8cfec191370017a6d16dcb07c077e0f0ecf484f1
b1tt <46069798+b1tt0@users.noreply.github.com>
2021-05-15 20:18:29 +0300

    ye

--- src/modules/mailbox.ts
+++ src/modules/mailbox.ts
@@ -2,17 +2,18 @@
 import { SMTPServer }   from "smtp-server";
 import { simpleParser } from "mailparser";
 import { util }         from "../util";
-import { writeFile } from "fs";
+import { writeFile }    from "fs";
 import config           from "../config.json";
 
 const server: SMTPServer = new SMTPServer({
   onData(stream, session, callback) {
     simpleParser(stream, {}, (e, p) => {
       if (e) return util.mailLog(`err: ${e}`);
+
       util.mailLog(`Received E-Mail from ${p.from.text} regarding: ${p.subject}`);
       const mail = `${p.from.text} wrote:\n\n---------------------\n${p.subject}\n\n${p.text}\n\n${p.attachments.map(a => a)}`;
       writeFile(`./data/mail/${p.to.text}/${p.date.toISOString()}.txt`, mail, (err): void => {
-        if(err) console.log(err);
+        err ? console.log(err) : null;
       });
       stream.on("end", callback);
     })
