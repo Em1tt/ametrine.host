72698e31e3c17a21ed57bde4e9cb561d4750f45d
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-05-15 19:09:50 +0200

    Update mailbox.ts

--- src/modules/mailbox.ts
+++ src/modules/mailbox.ts
@@ -2,20 +2,22 @@
 import { SMTPServer }   from "smtp-server";
 import { simpleParser } from "mailparser";
 import { util }         from "../util";
+import { writeFile } from "fs";
 import config           from "../config.json";
 
 const server: SMTPServer = new SMTPServer({
   onData(stream, session, callback) {
     simpleParser(stream, {}, (e, p) => {
       if (e) return util.mailLog(`err: ${e}`);
-
-      util.mailLog(`"${p.subject}" from ${p.from.text} for ${p.to.text}`);
-      util.mailLog(`${p.text}`);
+      util.mailLog(`Received E-Mail from ${p.from.text} regarding: ${p.subject}`);
+      const mail = `${p.from.text} wrote:\n\n---------------------\n${p.subject}\n\n${p.text}\n\n${p.attachments.map(a => a)}`;
+      writeFile(`./data/mail/${p.to.text}/${p.date.toISOString()}.txt`, mail, (err): void => {
+        err;
+      });
       stream.on("end", callback);
     })
   },
-  disabledCommands: ["AUTH"],
-  authOptional: true
+  disabledCommands: ["AUTH"]
 });
 
 server.listen(config.mail.port, "0.0.0.0", () => util.mailLog("server started"));
