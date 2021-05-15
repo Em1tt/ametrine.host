585985053341ae7da1e85d87a0dd3edca85b2109
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-05-15 19:32:47 +0200

    Update mailbox.ts

--- src/modules/mailbox.ts
+++ src/modules/mailbox.ts
@@ -11,13 +11,13 @@ const server: SMTPServer = new SMTPServer({
       if (e) return util.mailLog(`err: ${e}`);
       util.mailLog(`"${p.subject}" from ${p.from.text}`);
       util.mailLog(`${p.text}`);
-      const mail = `${p.from.text} wrote:\n\n---------------------\n${p.subject}\n\n${p.text}\n\n${p.attachments.map(a => a)}`;
+      const mail = `${p.from.text} wrote:\n\n---------------------\nSubject: ${p.subject}\n\nText: ${p.text}\n\n${p.attachments.map(a => a)}`;
       fs.mkdir(`./data/mail/${p.to.text}`, (err) => {
-        if(err) console.log(err);
+        if(err && !fs.existsSync(`./data/mail/${p.to.text}`)) console.log(err);
       });
       setTimeout(()=>{
         fs.writeFile(`./data/mail/${p.to.text}/${p.date.toISOString().replace(new RegExp(":", "g"), ".")}`, mail, (err): void => {
-          console.log(err);
+          if(err) console.log(err);
         });
       },1000);
       stream.on("end", callback);
