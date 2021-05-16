70740e26447b31d39ad842cd4b7c3fa9aad92201
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-05-16 11:54:55 +0200

    Update mailbox.ts
    Enhanced mail logging and fixed up spacing as per request of @b1tt0

--- src/modules/mailbox.ts
+++ src/modules/mailbox.ts
@@ -2,23 +2,23 @@
 import { SMTPServer }   from "smtp-server";
 import { simpleParser } from "mailparser";
 import { util }         from "../util";
-import fs from "fs";
+import fs               from "fs";
 import config           from "../config.json";
 
 const server: SMTPServer = new SMTPServer({
   onData(stream, session, callback) {
     simpleParser(stream, {}, (e, p) => {
       if (e) return util.mailLog(`err: ${e}`);
-      util.mailLog(`"${p.subject}" from ${p.from.text}`);
+      util.mailLog(`"Received E-Mail from "${p.from.text}"`);
       const mail = `${p.from.text} wrote:\n\n---------------------\nSubject: ${p.subject}\n\nText: ${p.text}\n\n${p.attachments.map(a => a.content)}\n`;
-      fs.mkdir(`./data/mail/${p.to.text}`, (err) => {
-        if(err && !fs.existsSync(`./data/mail/${p.to.text}`)) console.log(err);
+      fs.mkdir(`./data/mail/${p.to.text}`, (err): void => {
+        if (err && !fs.existsSync(`./data/mail/${p.to.text}`)) return console.error(err);
       });
-      setTimeout(()=>{
-        fs.writeFile(`./data/mail/${p.to.text}/${p.date.toISOString().replace(new RegExp(":", "g"), ".")}`, mail, (err): void => {
-          if(err) console.log(err);
+      setTimeout(() => {
+        fs.writeFile(`./data/mail/${p.to.text}/${p.subject.replace(/^[^<>\\:;,?"*|/]+$/g, '')}${p.date.toDateString().replace(new RegExp(":", "g"), ".")}`, mail, (err): void => {
+          if (err) console.log(err);
         });
-      },1000);
+      }, 1000);
       stream.on("end", callback);
     })
   },
