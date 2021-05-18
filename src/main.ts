c9fcf164b76df46ee9f33b5219bcdd4257fed848
b1tt0 <thssthfx@gmail.com>
2021-05-18 17:06:00 +0300

    YYYEYEYEYEHSYEHDGSYSHDJEUDJSJFRJDJISIDSJDJ

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ for (const file of mfiles) {
   modules.set(file.replace(".js", ""),
               child_process.fork(`dist/modules/${file}`));
 }
+console.log(util.sreplace("test %0%", ["hi"]));
 util.log(util.sreplace(strings.start, [modules.size]));
 
 // "CLI"
--- src/util.ts
+++ src/util.ts
@@ -5,18 +5,14 @@ export const util = {
     // but thats the only solution that comes in my head!!
     // whatcha gonna do about that? huh???????
     // - b1tt, 17 May 2021
-
-    for (let i = 0; i < 9; i ++) {
-      text.includes(`%${i}%`) ?
-          text.replace(new RegExp(`%${i}%`, "g"), rep[i]) :
-          null;
+    for (let i = 0; i < 10; i ++) {
+      text = text.replace(new RegExp(`%${i}%`, "g"), rep[i]);
     }
-
     return text;
   },
 
-  log:        (text: string): void => console.log(`[backend] ${text}`),
+  log       : (text: string): void => console.log(`[backend] ${text}`),
   expressLog: (text: string): void => console.log(`[website] ${text}` ),
   discordLog: (text: string): void => console.log(`[discord] ${text}` ),
-  mailLog:    (text: string): void => console.log(`[mailbox] ${text}` )
+  mailLog   : (text: string): void => console.log(`[mailbox] ${text}` )
 };
