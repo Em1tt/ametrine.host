2e8b3e350bba53754d0c595474e5d986b7b8c58a
Fire <17692105+FireMario211@users.noreply.github.com>
2021-05-24 12:58:36 +0000

    Add new table "sessions"

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -1,10 +1,12 @@
 -- initialize all tables
 CREATE TABLE IF NOT EXISTS users (
-  user_id    INTEGER   NOT NULL PRIMARY KEY,
-  registered TIMESTAMP NOT NULL,
-  name       TEXT      NOT NULL,
-  password   TEXT      NOT NULL,
-  salt       TEXT      NOT NULL
+  user_id    INTEGER   NOT NULL PRIMARY KEY, -- The Users ID
+  registered TIMESTAMP NOT NULL,             -- When the user registered
+  name       TEXT      NOT NULL,             -- The users real name
+  email      TEXT      NOT NULL,             -- For contacting the user
+  password   TEXT      NOT NULL,             -- Required
+  salt       TEXT      NOT NULL,             -- Extra Security, this will be used as an extra salt
+  verified   INTEGER   NOT NULL DEFAULT 0    -- If the user verified their email (1) or if they verified their phone # (2)
 );
 
 CREATE TABLE IF NOT EXISTS servers (
@@ -20,4 +22,14 @@ CREATE TABLE IF NOT EXISTS invoices (
   due         TIMESTAMP NOT NULL,
   price       REAL      NOT NULL DEFAULT 0.00,
   currency    TEXT      NOT NULL DEFAULT '€'
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+  session_id  INTEGER   NOT NULL PRIMARY KEY, -- Session ID
+  user_id     INTEGER   NOT NULL,             -- User ID
+  jwt         TEXT      NOT NULL,             -- JWT token
+  createdIn   TIMESTAMP NOT NULL,             -- When the Token was created
+  expiresIn   TIMESTAMP NOT NULL,             -- When the token expires
+  ip          TEXT      NOT NULL              -- Remote Address
+  rememberMe  INTEGER   NOT NULL DEFAULT 0    -- Will change what expiresIn should be
 );
