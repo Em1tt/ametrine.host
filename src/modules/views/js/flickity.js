1abf1eb7692da85dbbd430ca655a3334bbe62c8e
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-06-02 19:29:10 +0200

    Add files via upload

--- src/modules/views/js/flickity.js
+++ src/modules/views/js/flickity.js
@@ -1,10 +1,11 @@
-$('.carousel').flickity({
-  cellAlign: 'center',
-  contain: true,
-  groupCells: true,
-  wrapAround: true,
-  freeScroll: true,
-  prevNextButtons: false,
-  pageDots: false,
-  autoPlay: 5000
-});
+$(".carousel").flickity({
+    cellSelector: ".carousel-cell",
+    cellAlign: 'center',
+    contain: true,
+    groupCells: true,
+    wrapAround: true,
+    freeScroll: true,
+    prevNextButtons: false,
+    pageDots: false,
+    autoPlay: 5000
+  });
