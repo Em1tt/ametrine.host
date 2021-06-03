555665e7a69e3cc66e1fc642ad59aa170c3f82e2
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-06-03 16:44:13 +0200

    Update particles.js

--- src/modules/views/js/particles.js
+++ src/modules/views/js/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 250,
+      "value": 100,
       "density": {
         "enable": true,
         "value_area": 789.1476416322727
