72d79a155b8b35408c108c4a6699c07fe7bd3f84
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-06-04 17:32:05 +0200

    Potential lag fix for website
    Enabled Debug mode to show FPS on phone and PC

--- src/modules/views/js/particles.js
+++ src/modules/views/js/particles.js
@@ -1,72 +1,18 @@
-particlesJS("particles-js", {
-  "fps_limit": 1,
-  "particles": {
-    "number": {
-      "value": 75,
-      "density": {
-        "enable": true,
-        "value_area": 789.1476416322727
-      }
+new SuperParticles({
+    maxFPS: 30,
+    useJquery: true,
+    debug: {
+      showFps: true
     },
-    "color": {
-      "value": "#ffffff"
+    particles: {
+      amount: window.outerWidth / 13,
+      velocity: 10,
+      color: "0xdce0ff",
     },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
+    lines: {
+      minDistance: 0
     },
-    "opacity": {
-      "value": 0.48927153781200905,
-      "random": false,
-      "anim": {
-        "enable": true,
-        "speed": 0.2,
-        "opacity_min": 0,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 2,
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 2,
-        "size_min": 0,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-    },
-    "move": {
-      "enable": true,
-      "speed": 0.2,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
+    container: {
+      backgroundCssRule: null
     }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false
-      },
-      "onclick": {
-        "enable": false
-      },
-      "resize": true
-    }
-  },
-  "retina_detect": true
-});
+  })
