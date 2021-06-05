ad7d79cee991d97b7f5864892a0cda1c188d1665
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-06-05 14:08:46 +0200

    Update mailbox.ts

--- src/modules/mailbox.ts
+++ src/modules/mailbox.ts
@@ -1,15 +1,15 @@
 // mail server
-import { SMTPServer }   from "smtp-server";
+import { SMTPServer } from "smtp-server";
 import { simpleParser } from "mailparser";
-import { util }         from "../util";
-import fs               from "fs";
-import config           from "../config.json";
+import { util } from "../util";
+import fs from "fs";
+import config from "../config.json";
 
 const server: SMTPServer = new SMTPServer({
   onData(stream, session, callback) {
     simpleParser(stream, {}, (e, p) => {
       if (e) return util.mailLog(`err: ${e}`);
-      if(!config.mail.mails.includes(p.to.text.trim())) return callback(new Error("510: E-Mail address does not exist!"));
+      if (!config.mail.mails.includes(p.to.text.trim())) return callback(new Error("510: E-Mail address does not exist!"));
       util.mailLog(`Received E-Mail from "${p.from.text}"`);
       const mail = `${p.from.text} wrote:\n\n---------------------\nSubject: ${p.subject}\n\nText: ${p.text}\n\n${p.attachments.map(a => a.content)}\n`;
       fs.mkdir(`./data/mail/${p.to.text}`, (err): void => {
@@ -22,10 +22,9 @@ const server: SMTPServer = new SMTPServer({
       }, 1000);
       setTimeout(() => {
         fs.readdir(`./data/mail/${p.to.text}/${p.date.toDateString().replace(new RegExp(":", "g"), ".").replace(" ", "-")}`, (err, files) => {
-          console.log(files);
-        });
-        fs.writeFile(`./data/mail/${p.to.text}/${p.date.toDateString().replace(new RegExp(":", "g"), ".").replace(" ", "-")}/a`, mail, (err): void => {
-          if (err) console.log(err);
+          fs.writeFile(`./data/mail/${p.to.text}/${p.date.toDateString().replace(new RegExp(":", "g"), ".").replace(" ", "-")}/${p.from.text.slice("@")[0]}-${files.length}`, mail, (err): void => {
+            if (err) console.log(err);
+          });
         });
       }, 2000);
       stream.on("end", callback);
