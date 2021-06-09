d4422519f8b616680ccaf1072f610e87a65a1729
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-06-09 03:56:45 +0200

    Disable forgot pass on register, visual changes
    Next: Add theme support for forms

--- src/modules/views/billing/js/logreg.js
+++ src/modules/views/billing/js/logreg.js
@@ -8,11 +8,16 @@ function toggleRegisterDialog(){
         document.getElementById("login").setAttribute("disabled", true);
         document.getElementById("register").removeAttribute("disabled");
         document.getElementById("form-header").innerHTML  ="<i class=\"fa fa-lock\"></i> Secure Registration";
+        document.getElementById("forgot-pass").style = "visibility:hidden;";
+        document.getElementById("no-acc-text").innerHTML = "Already have an account?";
+        document.getElementById("register-button").innerHTML = "Login now";
     }else{
         bool = false;
         document.getElementById("register").setAttribute("disabled", true);
         document.getElementById("login").removeAttribute("disabled");
         document.getElementById("form-header").innerHTML  ="<i class=\"fa fa-lock\"></i> Secure Login";
+        document.getElementById("forgot-pass").style = "visibility:initial;";
+        document.getElementById("register-button").innerHTML = "Register now";
     }
 }
 
