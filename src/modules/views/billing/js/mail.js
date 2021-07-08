3b0ba4a6f27effb31efa8f4d6fd292e5dee525d6
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-07-08 18:14:55 +0200

    Made backend mail errors show up in frontend

--- src/modules/views/billing/js/mail.js
+++ src/modules/views/billing/js/mail.js
@@ -6,21 +6,22 @@ function onLoadMail() {
         const email = mailerForm.querySelector('#mailEmail').value
         const subject = mailerForm.querySelector('#mailSubject').value;
         const content = mailerForm.querySelector('#mailText').value;
+        const mailError = document.querySelector("#mailError");
         try {
             const response = await axios.post(url, { email, subject, content })
-            console.log(response)
+            console.log(response);
             // Add some sort of success text here, or use location.reload() if you want it to reload.
         } catch (e) {
             let errorText = "Unknown Error (Look in Console for more details)";
             switch (e.toString()) {
-                case "Error: Request failed with status code 403":
-                    errorText = "Invalid Permissions or Invalid User Token."; // Only happens if user isn't a client.
-                    break;
                 case "Error: Request failed with status code 405":
                     errorText = "Invalid Email."; // Could also be due to being the wrong method, though email being incorrect should most likely be the case.
                     break;
+                case "Error: Request failed with status code 403":
+                    errorText = "Invalid Permissions or not logged in. Please try again later."; // Only happens if user isn't a client.
+                    break;
             }
-            //error.innerHTML = `${exclamationCircle} ${errorText}`;
+            mailError.innerHTML = `<i class='fa fa-exclamation-circle'></i> ${errorText}`;
             console.error(e);
             console.log(errorText)
         }
