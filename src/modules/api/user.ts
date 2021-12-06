9f8699739a1ca0d96b54d2bfe7f307c69f28280c
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2021-12-05 22:22:25 -0500

    Added Password changing

--- src/modules/api/user.ts
+++ src/modules/api/user.ts
@@ -27,6 +27,7 @@ export const prop = {
         const params = req.params[0].split("/").slice(1); // Probably a better way to do this in website.ts via doing /api/:method/:optionalparam*? but it doesnt work for me.
         res.set("Allow", allowedMethods.join(", ")); // To give the method of whats allowed
         if (!allowedMethods.includes(req.method)) return res.sendStatus(405) // If the request isn't included from allowed methods, then respond with Method not Allowed.
+        //let userData = await auth.verifyToken(req, res, false, "access");
         let userData = await auth.verifyToken(req, res, false, "access");
         if (userData == 101) {
             const newAccessToken = await auth.regenAccessToken(req, res);
@@ -74,45 +75,73 @@ export const prop = {
                     switch (req.method) {
                         case "PUT": { // Updating user data, such as first name, email, etc
                             let user = await client.db.hmget(`user:${userData["user_id"]}`, ["password", "salt"])
-                            if (!user) return res.sendStatus(403);
+                            if (!user) return res.status(404).send("Cannot find user.");
                             user = { password: user[0], salt: user[1] };
-                            const { name, email, password } = req.body;
-                            if (!name && !email && !password) return res.sendStatus(406) // If nothing is provided, or invalid body was provided, bye.
-                            let updated = false;
-                            const verifyHash = await auth.verifyPassword(password, user);
-                            if (!verifyHash) return res.status(403).send("Incorrect Password.");
-                            if (name && name.length && name != userData["name"]) {
-                                const nameRes = await client.db.hset([`user:${userData["user_id"]}`, "name", name])
-                                if (nameRes != 0) return res.status(500).send("Error occured while changing name. Please report this.")
-                                updated = true;
-                            }
-                            if (email && email.length && email != userData["email"]) {
-                                const emailUsed = await client.db.hexists('users.email', email);
-                                if (emailUsed) return res.status(403).send("Email is already being used.");
-                                const emailRes = await client.db.hset([`user:${userData["user_id"]}`, "email", email])
-                                if (emailRes != 0) return res.status(500).send("Error occured while changing email. Please report this.")
-                                await client.hdel('users.email', userData["email"], function(err, emailRes2) {
-                                    if (!emailRes2) return console.error(err);
-                                })
-                                await client.db.hset([`users.email`, email, 1])
-                                updated = true;
-                            }
-                            // Commented below due to password changing not implemented in front end, will need another field (confirmPassword)
-                            /*if (password && password.length) { // Would really want to implement a ratelimit soon, since this could easily stress our servers.
-                                const user = await sql.db.prepare('SELECT password, salt FROM users WHERE user_id = ?').get(userData["user_id"]);
-                                if (!user) return res.sendStatus(404);
-                                const passResult = await auth.setPassword(password);
+                            const { name, email, password, passwordNew, passwordConfirm } = req.body;
+
+                            if (name && email) { // Updating user info
+                                if (!name && !email && !password) return res.status(406).send("Please insert either a name, email. And also insert a password.") // If nothing is provided, or invalid body was provided, bye.
+                                let updated = false;
+                                const verifyHash = await auth.verifyPassword(password, user);
+                                if (!verifyHash) return res.status(403).send("Incorrect Password.");
+                                if (name && name.length && name != userData["name"]) {
+                                    const nameRes = await client.db.hset([`user:${userData["user_id"]}`, "name", name])
+                                    if (nameRes != 0) return res.status(500).send("Error occured while changing name. Please report this.")
+                                    updated = true;
+                                }
+                                if (email && email.length && email != userData["email"]) {
+                                    const emailUsed = await client.db.hexists('users.email', email);
+                                    if (emailUsed) return res.status(403).send("Email is already being used.");
+                                    const emailRes = await client.db.hset([`user:${userData["user_id"]}`, "email", email])
+                                    if (emailRes != 0) return res.status(500).send("Error occured while changing email. Please report this.")
+                                    await client.hdel('users.email', userData["email"], function(err, emailRes2) {
+                                        if (!emailRes2) return console.error(err);
+                                    })
+                                    await client.db.hset([`users.email`, email, 1])
+                                    updated = true;
+                                }
+                                if (!updated) return res.sendStatus(202);
+                                await auth.regenAccessToken(req, res);
+                                return res.sendStatus(200);
+                            } else { // Updating password
+                                const { refreshToken } = await auth.verifyToken(req, res, false, "refresh");
+                                if (!refreshToken) return res.sendStatus(403);
+                                const sessionID = await client.db.hget("sessions.jwtid", `${refreshToken}:${userData["user_id"]}`);
+                                if (!sessionID) return res.sendStatus(403);
+
+                                if (!password || !passwordNew || !passwordConfirm) return res.status(406).send("Please insert the passwords in the password fields!")
+                                const verifyHash = await auth.verifyPassword(password, user);
+                                if (!verifyHash) return res.status(403).send("Incorrect Password.");
+                                if (passwordNew != passwordConfirm) return res.status(406).send("The new password doesn't match the confirm password field!") // Could do this instead in client side.
+                                const passResult = await auth.setPassword(passwordNew);
                                 if (passResult.result && passResult.result == 406) return res.status(406)
                                                                                              .send("Password must not be less than 6 characters.");
-                                const verifyHash = await auth.verifyPassword(password, user);
-                                if (verifyHash) return res.status(403).send("You can't change it to the same password.");
+                                const verifyNewHash = await auth.verifyPassword(passwordNew, user);
+                                if (verifyNewHash) return res.status(403).send("You can't change it to the same password.");
+                                const passwordRes = await client.db.hset([`user:${userData["user_id"]}`, "password", passResult.password, "salt", passResult.salt])
+                                if (passwordRes != 0) return res.status(500).send("Error occured while changing the password. Please report this.");
                                 
-                                await sql.db.prepare('UPDATE users SET password = ?, salt = ? WHERE user_id = ?').run(passResult.password, passResult.salt, userData["user_id"]);
-                                updated = true;
-                            }*/
-                            if (!updated) return res.sendStatus(202);
-                            await auth.regenAccessToken(req, res);
-                            return res.sendStatus(200);
+                                const getAllSessions: Record<string, unknown> = await client.db.hgetall(`sessions.jwtid`); // May find another solution to this, as this could cost performance.
+                                if (!getAllSessions) return res.status(500).send("Sessions not found.");
+                                const userSessions = Object.keys(getAllSessions).filter(session => session.split(":")[1] == userData["user_id"] && session != `${refreshToken}:${userData["user_id"]}`); // Get all sessions besides user
+                                if (!userSessions.length) return res.status(404).send("User sessions not found.")
+                                userSessions.map(session => {
+                                    client.del(`session:${getAllSessions[session]}`, function(err) {
+                                        if (err) {
+                                            console.error(err);
+                                            return res.status(500).send("Error occurred while deleting the sessions. Please report this.")
+                                        }
+                                        return client.hdel(`sessions.jwtid`, `${session}:${userData["user_id"]}`, function (err2) {
+                                            if (err2) {
+                                                console.error(err2);
+                                                return res.status(500).send("Error occured while deleting the session. Please report this.")
+                                            }
+                                            return res.sendStatus(200); // OK
+                                        })
+                                    })
+                                })
+                            }
+                            break;
                         }
                         case "DELETE": // Deletes the account.
 
