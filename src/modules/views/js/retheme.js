776bf4cd0bc9532e7bf04609853ab654d84b7e56
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2021-12-30 08:12:25 +0100

    Fixed bug caused by 9d1d5a2 causing the need for double input on theme change

--- src/modules/views/js/retheme.js
+++ src/modules/views/js/retheme.js
@@ -1,6 +1,9 @@
 let theme = window.localStorage.getItem("theme");
 const iconSwitch = [...document.getElementsByClassName("iconSwitch")];
 if(theme || window.matchMedia("(prefers-color-scheme: dark)").matches){
+    if(!theme){
+        window.localStorage.setItem("theme", "dark");
+    }
     document.body.classList.add("dark");
     iconSwitch.forEach(icon => {
         icon.classList.remove("fa-moon");
@@ -8,8 +11,9 @@ if(theme || window.matchMedia("(prefers-color-scheme: dark)").matches){
     })
 }
 function retheme(){
-    let curTheme = window.localStorage.getItem("theme");
-    if(curTheme){
+    console.log("hi");
+    let hasClass = document.body.classList.contains("dark");
+    if(hasClass){
         window.localStorage.removeItem("theme");
         document.body.classList.remove("dark");
         iconSwitch.forEach(icon => {

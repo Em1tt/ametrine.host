Em1tt/ametrine.host

commit 9d1d5a243df56021036730ff77169526882b81ad

Quality of Life for themes - main site
TLDR: Website theme matches user's system theme;

Whenever a user has dark mode set in their OS, or their browser, the website will favour that on the same level it favours a cookie, instantly setting the website into a dark mode environment

src/modules/views/js/retheme.js
@@ -1,6 +1,6 @@
 let theme = window.localStorage.getItem("theme");
 const iconSwitch = [...document.getElementsByClassName("iconSwitch")];
-if(theme){
+if(theme || window.matchMedia("(prefers-color-scheme: dark)").matches){
     document.body.classList.add("dark");
     iconSwitch.forEach(icon => {
         icon.classList.remove("fa-moon");

commit 776bf4cd0bc9532e7bf04609853ab654d84b7e56

Fixed bug caused by 9d1d5a2 causing the need for double input on theme change

src/modules/views/js/retheme.js
@@ -1,6 +1,9 @@
 let theme = window.localStorage.getItem("theme");
 const iconSwitch = [...document.getElementsByClassName("iconSwitch")];
 if(theme || window.matchMedia("(prefers-color-scheme: dark)").matches){
+    if(!theme){
+        window.localStorage.setItem("theme", "dark");
+    }
     document.body.classList.add("dark");
     iconSwitch.forEach(icon => {
         icon.classList.remove("fa-moon");
@@ -8,8 +11,9 @@ if(theme || window.matchMedia("(prefers-color-scheme: dark)").matches){
     })
 }
 function retheme(){
-    let curTheme = window.localStorage.getItem("theme");
-    if(curTheme){
+    console.log("hi");
+    let hasClass = document.body.classList.contains("dark");
+    if(hasClass){
         window.localStorage.removeItem("theme");
         document.body.classList.remove("dark");
         iconSwitch.forEach(icon => {