437013d6dcb0abe6cb76c8c3c6463a0111826505
b1tt0 <thssthfx@gmail.com>
2022-01-03 01:54:17 +0300

    [backend] use spawn instead of fork

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ const strings   : any    = require(`./lang/${config.lang}.json`),
 
 for (const file of moduleList) {
   modules.set(file.name,
-              child_process.fork(file.file));
+              child_process.spawn(`node ${file.file}`));
 }
 
 util.log(util.sreplace(strings.start, [modules.size]));
