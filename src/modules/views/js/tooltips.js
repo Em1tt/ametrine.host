afa55365f8f7bb0564370c7e4d8942523f3ba940
Richard <55552821+Em1tt@users.noreply.github.com>
2022-03-19 12:59:05 +0100

    Added some accessibility buttons for main site

--- src/modules/views/js/tooltips.js
+++ src/modules/views/js/tooltips.js
@@ -1,10 +1,14 @@
 const text = [...document.getElementsByClassName("poi")];
-let i = 0;
-text.forEach(t => {
-    tippy(t, {
+text.forEach((t, i) => {
+    const tooltip = tippy(t, {
         content: ["<b>Finland</b><br><sup>Helsinki</sup>", "<b>Germany</b><br><sup>Falkenstein</sup>"][i],
         placement: "top",
         allowHTML: true
-    })
-    i++;
+    });
+    document.getElementById([...document.getElementById("map-buttons-wrapper").children][i].id).addEventListener("mouseenter", () => {
+        tooltip.show();
+    });
+    document.getElementById([...document.getElementById("map-buttons-wrapper").children][i].id).addEventListener("mouseleave", () => {
+        tooltip.hide();
+    });
 })
