43d2e84d66f8395270c561b5e8ecfdad4c6b686c
FireMario211 <17692105+FireMario211@users.noreply.github.com>
2022-04-28 18:39:49 -0400

    Added PUT /api/users/:userid

--- src/modules/api/users.ts
+++ src/modules/api/users.ts
@@ -61,8 +61,8 @@ export const prop = {
             if ([3,4].includes(userObj.state)) return res.status(406).send("The account is currently either disabled or terminated.")
             // Deleting all sessions
             let getAllSessions = await client.db.hgetall(`sessions.jwtid`); // May find another solution to this, as this could cost performance.
-            const userSessions = Object.keys(getAllSessions).filter(session => session.split(":")[1] == userObj.user_id); // Get all sessions besides user
-            if (!userSessions.length) getAllSessions = []; // No other sessions found besides the users.
+            const userSessions = Object.keys(getAllSessions).filter(session => session.split(":")[1] == userObj.user_id); // Get all sessions.
+            if (!userSessions.length) getAllSessions = []; // No other sessions found.
             auth.logoutAll(userObj.user_id, userSessions, getAllSessions).then(() => {
                 if (req.body.force != undefined) {
                     return client.del(`user:${userObj.user_id}`, function (err) {
@@ -125,11 +125,104 @@ export const prop = {
                         switch (req.method) {
                             case "GET": // gets the users data
                                 return res.status(200).json(showUserData(getUser));
-                            case "PUT": // Edits user information such as their name, email, clearing sessions, or password
-                                return res.sendStatus(501);
-                                // not finished
-                                break;
+                            case "PUT": { // Edits user information such as their name, email, clearing sessions, or password
+                                if (parseInt(getUser["permission_id"] as string) >= parseInt(userData["permission_id"])) return res.status(403).send("Due to the user having a higher or equal permission as you, you aren't able to change their details!"); // You can change this message
+                                const { name, email, password, passwordConfirm } = req.body;
+                                if (name && email) {
+                                    let updated = false;
+                                    if (name && name.length && name != getUser["name"]) {
+                                        const nameRes = await client.db.hset([`user:${userID}`, "name", name]);
+                                        if (nameRes != 0) return res.status(500).send("Error occured while changing the name of the user. Please report this.");
+                                        updated = true;
+                                    }
+                                    if (email && email.length && email != getUser["email"]) {
+                                        const { email } = req.body;
+                                        const emailUsed = await client.db.hexists('users.email', email);
+                                        if (emailUsed) return res.status(403).send("Email is already being used.");
+                                        const emailRes = await client.db.hset([`user:${userID}`, "name", email]);
+                                        if (emailRes != 0) return res.status(500).send("Error occured while changing email. Please report this.")
+                                        client.hdel('users.email', userData["email"], async function(err, emailRes2) {
+                                            if (!emailRes2 || err) return console.error(err);
+                                            const emailRes3 = await client.db.hset([`users.email`, email, 1])
+                                            if (emailRes3 != 0) return res.status(500).send("Error occured while changing the claimed emails. Please report this.")
+                                        })
+                                        updated = true;
+                                    }
+                                    if (!updated) return res.sendStatus(202);
+                                    return res.sendStatus(204);
+                                } else { // Changing password. This will also log the user out
+                                    if (!password || !passwordConfirm) return res.status(406).send("Please insert the passwords in the password fields!")
+                                    if (password != passwordConfirm) return res.status(406).send("The new passwords don't match.") // Could do this instead in client side.
+                                    const passResult = await auth.setPassword(password);
+                                    if (passResult.result && passResult.result == 406) return res.status(406)
+                                                                                                 .send("New password must not be less than 6 characters.");
+                                    const verifyNewHash = await auth.verifyPassword(password, getUser);
+                                    if (verifyNewHash) return res.status(403).send("You can't change the password to the current password.");
+                                    const passwordRes = await client.db.hset([`user:${userID}`, "password", passResult.password, "salt", passResult.salt])
+                                    if (passwordRes != 0) return res.status(500).send("Error occured while changing the password. Please report this.");
+                                    let getAllSessions = await client.db.hgetall(`sessions.jwtid`); // May find another solution to this, as this could cost performance.
+                                    if (!getAllSessions) return res.status(500).send("Sessions not found.");
+                                    const userSessions = Object.keys(getAllSessions).filter(session => session.split(":")[1] == getUser["user_id"]); // Get all sessions
+                                    if (!userSessions.length) getAllSessions = []; // No other sessions found
+                                    return auth.logoutAll(userID, userSessions, getAllSessions).then(() => {
+                                        return res.sendStatus(204)
+                                    }).catch(err => {
+                                        console.error(err);
+                                        return res.status(500).send(err);
+                                    })
+                                }
+
+                                /*const { category } = req.body;
+                                switch (category) {
+                                    case "name": {
+                                        const { name } = req.body;
+                                        const nameRes = await client.db.hset([`user:${userID}`, "name", name]);
+                                        if (nameRes != 0) return res.status(500).send("Error occured while changing the name of the user. Please report this.");
+                                        return res.sendStatus(204);
+                                    }
+                                    case "email": {
+                                        const { email } = req.body;
+                                        const emailUsed = await client.db.hexists('users.email', email);
+                                        if (emailUsed) return res.status(403).send("Email is already being used.");
+                                        const emailRes = await client.db.hset([`user:${userID}`, "name", email]);
+                                        if (emailRes != 0) return res.status(500).send("Error occured while changing email. Please report this.")
+                                        return client.hdel('users.email', userData["email"], async function(err, emailRes2) {
+                                            if (!emailRes2 || err) return console.error(err);
+                                            const emailRes3 = await client.db.hset([`users.email`, email, 1])
+                                            if (emailRes3 != 0) return res.status(500).send("Error occured while changing the claimed emails. Please report this.")
+                                            return res.sendStatus(204);
+                                        })
+                                    }
+                                    case "password": { // This will also log the user out
+                                        const { password, passwordConfirm } = req.body;
+                                        
+
+                                        if (password != passwordConfirm) return res.status(406).send("The new passwords don't match.") // Could do this instead in client side.
+                                        const passResult = await auth.setPassword(password);
+                                        if (passResult.result && passResult.result == 406) return res.status(406)
+                                                                                                     .send("New password must not be less than 6 characters.");
+                                        const verifyNewHash = await auth.verifyPassword(password, getUser);
+                                        if (verifyNewHash) return res.status(403).send("You can't change the password to the current password.");
+                                        const passwordRes = await client.db.hset([`user:${userID}`, "password", passResult.password, "salt", passResult.salt])
+                                        if (passwordRes != 0) return res.status(500).send("Error occured while changing the password. Please report this.");
+                                        let getAllSessions = await client.db.hgetall(`sessions.jwtid`); // May find another solution to this, as this could cost performance.
+                                        if (!getAllSessions) return res.status(500).send("Sessions not found.");
+                                        const userSessions = Object.keys(getAllSessions).filter(session => session.split(":")[1] == getUser["user_id"]); // Get all sessions
+                                        if (!userSessions.length) getAllSessions = []; // No other sessions found
+                                        return auth.logoutAll(userID, userSessions, getAllSessions).then(() => {
+                                            return res.sendStatus(204)
+                                        }).catch(err => {
+                                            console.error(err);
+                                            return res.status(500).send(err);
+                                        })
+                                    }
+                                   
+                                    default:
+                                        return res.status(406).send("Enter in a category!");
+                                }*/
+                            }
                             case "PATCH": { // Edits user information such as toggling 2fa, or permission id
+                                if (parseInt(getUser["permission_id"] as string) >= parseInt(userData["permission_id"])) return res.status(403).send("Due to the user having a higher or equal permission as you, you aren't able to change their details!"); // You can change this message
                                 const { category } = req.body;
                                 switch (category) {
                                     case "2fa": {
@@ -148,8 +241,21 @@ export const prop = {
                                         const { id } = req.body;
                                         if (!permissions.isAdminPermission(userData['permission_id']) && parseInt(id) >= parseInt(userData["permission_id"])) return res.status(403).send("You can't set a permission higher than or equal as your own!");
                                         if (!permissions.validPermission(id)) return res.status(406).send("Invalid permission ID.");
-                                        await client.db.hset([`user:${userID}`, "permission_id", id]);
+                                        const permRes = await client.db.hset([`user:${userID}`, "permission_id", id]);
+                                        if (permRes != 0) return res.status(500).send("Error occured while changing the permission ID. Please report this.");
                                         return res.sendStatus(204);
+                                    }
+                                    case "sessions": {
+                                        let getAllSessions = await client.db.hgetall(`sessions.jwtid`); // May find another solution to this, as this could cost performance.
+                                        if (!getAllSessions) return res.status(500).send("Sessions not found.");
+                                        const userSessions = Object.keys(getAllSessions).filter(session => session.split(":")[1] == getUser["user_id"]); // Get all sessions
+                                        if (!userSessions.length) getAllSessions = []; // No other sessions found
+                                        return auth.logoutAll(userID, userSessions, getAllSessions).then(() => {
+                                            return res.sendStatus(204);
+                                        }).catch(err => {
+                                            console.error(err);
+                                            return res.status(500).send(err);
+                                        })
                                     }
                                     default:
                                         return res.status(406).send("Enter in a category!");
@@ -174,7 +280,7 @@ export const prop = {
                             const uIDs       : Array<number> = [],
                                   permissions: Array<string> = [],
                                   emails     : Array<string> = [];
-                            if(req.query.filter){
+                            if (req.query.filter){
                                 req.query.filter.split(",").forEach(filter => {
                                     if(!isNaN(parseInt(filter))) return uIDs.push(parseInt(filter));
                                     if(filter.split("=")[0].toLowerCase() == "perm") return permissions.push(filter.split("=")[1]);
