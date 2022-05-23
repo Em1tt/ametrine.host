9136b61fed137587192637a07c6be8fbd7d05a7d
Richard <55552821+Em1tt@users.noreply.github.com>
2022-05-23 16:48:59 +0200

    Pushing frontend

--- src/modules/views/billing/html/staff/javascripts/knowledgebase.js
+++ src/modules/views/billing/html/staff/javascripts/knowledgebase.js
@@ -1,13 +1,29 @@
 (async () => {
   try{
-    let button = document.getElementById("article-new")
-    button.addEventListener("click", async (e) => {
-      button.setAttribute("disabled", true);
-      const response = await axios.post(`/api/knowledgebase/create`, {
-        tags: "unfinished"
+    let button = document?.getElementById("article-new");
+    if(button){
+      button.addEventListener("click", async (e) => {
+        button.setAttribute("disabled", true);
+        const response = await axios.post(`/api/knowledgebase/create`, {
+          tags: "unfinished"
+        });
+        window.location.href = `/billing/staff/knowledgebase/${response.data.article_id}/editor`;
       });
-      window.location.href = `/billing/staff/knowledgebase/${response.data.article_id}/editor`;
-    });
+    }
+    let button2 = document?.getElementById("articles");
+    if(button2){
+      button2.addEventListener("click", async (e) => {
+        button.setAttribute("disabled", true);
+        window.location.href = `/billing/staff/knowledgebase/articles`;
+      });
+    }
+    let button3 = document?.getElementById("categories");
+    if(button3){
+      button3.addEventListener("click", async (e) => {
+        button.setAttribute("disabled", true);
+        window.location.href = `/billing/staff/knowledgebase/categories`;
+      });
+    }
   }catch(e){
     console.error(e);
   }
